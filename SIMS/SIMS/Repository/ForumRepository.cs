7d042844b434facd592028d9dc1dacc5418c64a4
Zuba13 <116648055+Zuba13@users.noreply.github.com>
2023-06-11 19:34:07 +0200

    feature/forum added forum to project

---
+++ SIMS/SIMS/Repository/ForumRepository.cs
@@ -0,0 +1,57 @@
+﻿using SIMS.Domain.Model;
+using SIMS.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS.Repository
+{
+    public class ForumRepository
+    {
+        private const string _filePath = "../../../../SIMS/Resources/Data/Forums.csv";
+        private readonly Serializer<Forum> _serializer;
+
+        private readonly AccommodationRepository _accommodationRepository;
+        private readonly UserRepository _userRepository;
+
+        private List<Forum> _forums;
+
+        public ForumRepository()
+        {
+            _serializer = new Serializer<Forum>();
+            _accommodationRepository = new AccommodationRepository();
+            _userRepository = new UserRepository();
+            _forums = new List<Forum>();
+        }
+
+        public int GetNextId(List<Forum> forums)
+        {
+            if (forums.Count < 1)
+            {
+                return 0;
+            }
+            return forums.Max(forum => forum.Id) + 1;
+        }
+
+        public List<Forum> GetAll()
+        {
+            _forums = _serializer.FromCSV(_filePath);
+            foreach (Forum forum in _forums)
+            {
+                forum.Accommodation = _accommodationRepository.GetById(forum.Accommodation.Id);
+                forum.ForumOwner = _userRepository.GetById(forum.ForumOwner.Id);
+            }
+            return _forums;
+        }
+
+        public Forum GetById(int id)
+        {
+            _forums = GetAll();
+            return _forums.FirstOrDefault(forum => forum.Id == id);
+        }
+
+
+    }
+}
--- SIMS/SIMS/WPF/ViewModel/Guest1ViewModel/RatingsViewModel.cs
+++ SIMS/SIMS/WPF/ViewModel/Guest1ViewModel/RatingsViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SIMS.WPF.ViewModel.Guest1ViewModel
 {
@@ -19,9 +20,15 @@ namespace SIMS.WPF.ViewModel.Guest1ViewModel
         public static ObservableCollection<GuestRating> GuestRatings { get; set; }
         public static ObservableCollection<OwnerRating> OwnerRatings { get; set; }
         public static ObservableCollection<User> Users { get; set; }
+
+        private ForumViewModel ForumVM;
         public GuestRating SelectedGuestRating { get; set; }
 
-        public RatingsViewModel(User user)
+
+        Guest1MainViewModel guest1MainViewModel;
+
+
+        public RatingsViewModel(User user, Guest1MainViewModel mvm)
         {
             _guestRatingRepository = new GuestRatingRepository();
             _ownerRatingRepository = new OwnerRatingRepository();
@@ -29,6 +36,7 @@ namespace SIMS.WPF.ViewModel.Guest1ViewModel
             OwnerRatings = new ObservableCollection<OwnerRating>(_ownerRatingRepository.GetAll());
             GuestRatings = new ObservableCollection<GuestRating>(_guestRatingRepository.GetByUserId(user.Id));
             Users = new ObservableCollection<User>(_userRepository.GetAllUsers());
+            guest1MainViewModel = mvm;
             PopulateUsers();
             LoggedInUser = user;
         }
@@ -41,6 +49,22 @@ namespace SIMS.WPF.ViewModel.Guest1ViewModel
             {
                 OwnerRatings = new ObservableCollection<OwnerRating>(_ownerRatingRepository.GetByUserId(rating.User.Id));
                 rating.User = Users.FirstOrDefault(a => a.Id == rating.User.Id);
+            }
+        }
+
+
+        private ICommand _forumCommand;
+
+        public ICommand ForumCommand
+        {
+            get
+            {
+                return _forumCommand ?? (_forumCommand = new CommandBase(
+                    () =>
+                    {
+                        ForumVM = new ForumViewModel(LoggedInUser, guest1MainViewModel);
+                        guest1MainViewModel.CurrentView = ForumVM;
+                    }, true));
             }
         }
     }
